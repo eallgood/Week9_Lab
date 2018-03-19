16223240eaa6524c81bb07af0293ee37565e9ead
eallgood <32801558+eallgood@users.noreply.github.com>
2018-03-19 15:37:06 -0700

    forgot to include API

---
+++ MyAppApi/Controllers/PetsController.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Web.Http;
+using Week4_Lab.Data;
+using Week4_Lab.Data.Entities;
+
+namespace Week9_Lab.Controllers
+{
+    [RoutePrefix("api/pets")]
+    public class PetsController : ApiController
+    {
+        private AppDbContext _dbContext;
+
+        public PetsController()
+        {
+            _dbContext = new AppDbContext();
+        }
+
+        [HttpGet]
+        public IEnumerable<Pet> GetAllPets()
+        {
+            return _dbContext.Pets.ToList();
+        }
+
+        [Route("{id}")]
+        [HttpGet]
+        public IHttpActionResult GetPet(int id)
+        {
+            var pet = _dbContext.Pets.FirstOrDefault((p) => p.Id == id);
+            if (pet == null)
+            {
+                return NotFound();
+            }
+            return Ok(pet);
+        }
+    }
+}
---
+++ MyAppApi/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="MyAppApi.WebApiApplication" Language="C#" %>
